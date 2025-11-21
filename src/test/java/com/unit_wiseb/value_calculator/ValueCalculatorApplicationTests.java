278757e9ba21afe749c5a4a408e9bc0f13e40470
str-leshs <0805eslee@gmail.com>
2025-11-21 17:15:56 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/unit_wiseb/value_calculator/ValueCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit_wiseb.value_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
