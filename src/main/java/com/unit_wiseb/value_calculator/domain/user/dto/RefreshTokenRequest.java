8c8aa14f9d0e3b3636c9f0c0cc55e0055bcfb3e6
str-leshs <0805eslee@gmail.com>
2025-11-22 23:33:09 +0900

    feat: 리프레시 토큰 관련 dto 추가 및 yml수정

---
+++ src/main/java/com/unit_wiseb/value_calculator/domain/user/dto/RefreshTokenRequest.java
@@ -0,0 +1,11 @@
+package com.unit_wiseb.value_calculator.domain.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RefreshTokenRequest {
+
+    private String refreshToken;
+}
